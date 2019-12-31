09e38d324a99cd65fb08ee2e29df6a50250948c8
ClaireRimel <sivadiercl@gmail.com>
2019-12-31 19:01:03 +0100

    New: Prototype search screen

---
+++ Reciplease/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Claire on 31/12/2019.
+//  Copyright © 2019 Claire Sivadier. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet var ingredientsTextField: UITextField!
+    @IBOutlet var ingredientsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addIngredient() {
+        
+    }
+    
+    @IBAction func clearIngredients() {
+        
+    }
+
+}
