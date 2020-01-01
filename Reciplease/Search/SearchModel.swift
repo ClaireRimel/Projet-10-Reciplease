0dd803710261381a744856d8e8c2480ddf9fb579
ClaireRimel <sivadiercl@gmail.com>
2020-01-01 20:07:30 +0100

    Chg: refactor request with alamofire

--- Reciplease/Search/SearchModel.swift
+++ Reciplease/Search/SearchModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 // Only class can comforn to this protocol
 protocol SearchModelDelegate: class {
@@ -39,44 +40,67 @@ class SearchModel {
     }
     
     func request(then: @escaping (Result<[Recipe], SearchRecipesError>) -> Void) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "api.edamam.com"
-        urlComponents.path = "/search"
         
         let ingredients = arrayIngredients.joined(separator: " ")
+        
+        let parameters = ["q": ingredients, "app_id": "a674e9c4", "app_key": "35f93780dbf6f834909870f2529a9871"]
+        
+        AF.request("https://api.edamam.com/search", method: .get, parameters: parameters).responseJSON { response in
 
-        urlComponents.queryItems = [URLQueryItem(name: "q", value: ingredients),
-                                    URLQueryItem(name: "app_id", value: "a674e9c4"),
-                                    URLQueryItem(name: "app_key", value: "35f93780dbf6f834909870f2529a9871")]
-        
-        guard let url = urlComponents.url else {
-            fatalError("Invalid URL")
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            // Verifies if the request threw an error
-            if let error = error as NSError? {
-                DispatchQueue.main.async {
-                    then(.failure(.requestError(error)))
-                }
-                return
-            }
-
-            guard let data = data,
+            debugPrint(response)
+            
+            //errors ?
+//            response.error
+                        
+            guard let data = response.data,
                 let responseJSON = try? JSONDecoder().decode(SearchRecipesResponse.self, from: data) else {
                     DispatchQueue.main.async {
                         then(.failure(.invalidResponseFormat))
                     }
                     return
             }
-            
+
             print(responseJSON)
+
         }
-        task.resume()
+//        var urlComponents = URLComponents()
+//        urlComponents.scheme = "https"
+//        urlComponents.host = "api.edamam.com"
+//        urlComponents.path = "/search"
+//
+//        let ingredients = arrayIngredients.joined(separator: " ")
+//
+//        urlComponents.queryItems = [URLQueryItem(name: "q", value: ingredients),
+//                                    URLQueryItem(name: "app_id", value: "a674e9c4"),
+//                                    URLQueryItem(name: "app_key", value: "35f93780dbf6f834909870f2529a9871")]
+//
+//        guard let url = urlComponents.url else {
+//            fatalError("Invalid URL")
+//        }
+//
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//
+//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+//            // Verifies if the request threw an error
+//            if let error = error as NSError? {
+//                DispatchQueue.main.async {
+//                    then(.failure(.requestError(error)))
+//                }
+//                return
+//            }
+//
+//            guard let data = data,
+//                let responseJSON = try? JSONDecoder().decode(SearchRecipesResponse.self, from: data) else {
+//                    DispatchQueue.main.async {
+//                        then(.failure(.invalidResponseFormat))
+//                    }
+//                    return
+//            }
+//
+//            print(responseJSON)
+//        }
+//        task.resume()
     }
 }
 
